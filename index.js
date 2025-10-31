879ee1231f5b454fadee99bb24a49c769e982d23
Sumi Akter <sumiwdcse@gmail.com>
2025-11-01 00:39:12 +0600

    practice 54-6 Delete a user, handle objectId

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000;
@@ -45,6 +45,14 @@ async function run() {
       console.log('User unfo:', newUser);
       const result = await usersCollection.insertOne(newUser)
       res.send(result);
+    })
+    app.delete ('/users/:id', async(req,res)=>{
+      console.log(req.params)
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+
     })
 
 
